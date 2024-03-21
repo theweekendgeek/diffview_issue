138eb1f1512287515efb5fd09416b6c7e9d840d1
marcus.doescher <deh.marcus@posteo.net>
2024-03-21 12:51:02 +0100

    unwrap else branch

--- test.js
+++ test.js
@@ -19,37 +19,30 @@ export default class Example {
         let condition4;
         let condition5;
 
-        if (condition1 || condition2) {
-            // comment 0
-            someArray.push('some logic 1');
+        if (condition3) {
+            if (condition4) {
+                // comment 1
+                someArray.push('some logic 2');
+            } else {
+                // comment 2
+                someArray.push(
+                    'some logic 3'
+                );
+            }
         } else {
 
-            if (condition3) {
-                if (condition4) {
-                    // comment 1
-                    someArray.push('some logic 2');
-                } else {
-                    // comment 2
-                    someArray.push(
-                        'some logic 3'
-                    );
-                }
-            } else {
-
-                if (!condition5) {
-                    // comment 3
-                    someArray.push('some logic 4');
-                }
-
-                // comment 4
-                someArray.push('some logic 5');
+            if (!condition5) {
+                // comment 3
+                someArray.push('some logic 4');
             }
+            // comment 4
+            someArray.push('some logic 5');
+        }
 
-            let condition6;
-            if (condition6) {
-                // comment 5
-                someArray.push('some logic 6');
-            }
+        let condition6;
+        if (condition6) {
+            // comment 5
+            someArray.push('some logic 6');
         }
 
         return someArray
