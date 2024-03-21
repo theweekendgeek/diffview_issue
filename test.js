da11d87012546068e01e997a3e51ff79ba28668c
marcus.doescher <deh.marcus@posteo.net>
2024-03-21 12:49:46 +0100

    first commit

---
+++ test.js
@@ -0,0 +1,47 @@
+export default class Example {
+    static async exampleMethod() {
+        const someArray = []
+        let condition1;
+        let condition2;
+        let condition3;
+        let condition4;
+        let condition5;
+
+        if (condition1 || condition2) {
+            // comment 0
+            someArray.push('some logic 1');
+        } else {
+
+            if (condition3) {
+                if (condition4) {
+                    // comment 1
+                    someArray.push('some logic 2');
+                } else {
+                    // comment 2
+                    someArray.push(
+                        'some logic 3'
+                    );
+                }
+            } else {
+
+                if (!condition5) {
+                    // comment 3
+                    someArray.push('some logic 4');
+                }
+
+                // comment 4
+                someArray.push('some logic 5');
+            }
+
+            let condition6;
+            if (condition6) {
+                // comment 5
+                someArray.push('some logic 6');
+            }
+        }
+
+        return someArray
+    }
+
+}
+
